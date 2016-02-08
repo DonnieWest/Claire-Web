6f28286662e17564453d859f8ef25d082569b292
Jared L Jennings <jared@jaredjennings.org>
2016-02-07 20:48:22 -0600

    Layout updates and Functional Updates
    Deposit ratings only available when received

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,11 @@
 console.log('\'Hello \'Hello!');
-var apiDomainURL = "https://frank.treasury.love";
-// var apiDomainURL = "http://localhost:3000";
+var apiDomainURL = "";
+if (window.location.hostname === "localhost") {
+  apiDomainURL = "http://localhost:3000";
+} else {
+  apiDomainURL = "https://frank.treasury.love";
+}
+
 var profile_firstname = localStorage.getItem("profile_firstname");
 var linked_ID = localStorage.getItem("linked_profile_id");
 var linked_FirstName = localStorage.getItem("linked_profile_firstname");
@@ -24,62 +29,13 @@ if(linked_ID === null){
   $('#message').text('You must define a partner before you can submit an entry!').addClass('alert alert-warning').show();
 }
 
-$('#profile').submit(function() {
-  var firstName = $(this).find('input[name="firstname"]').val();
-  var lastName = $(this).find('input[name="lastname"]').val();
-  var email = $(this).find('input[name="email"]').val();
-  var dat = '{"data": {"type":"profiles", "attributes":{"firstname":"' + firstName + '", "lastname":"' + lastName +
-    '", "email":"' + email + '"}}}';
-  console.log(dat);
-  $.ajax({
-    type: $(this).attr('method'),
-    url: apiDomainURL + $(this).attr('action'), //submits it to the given url of the form
-    data: dat,
-    headers: {
-      Accept: "application/json",
-      'Content-Type': 'application/vnd.api+json'
-    },
-    dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
-  }).success(function(json){
-    console.log("JSON success response", json);
-    debugger;
-    localStorage.setItem("profile_id", json.data.id);
-    localStorage.setItem("profile_firstname", firstName);
-    $('#message').text('Success!').show();
-  }).error(function(json) {
-    console.log (json);
-    $('#message').text('Oops, there was a problem').show();
-  });
-
-  /* stop form from submitting normally */
-  event.preventDefault();
-});
-
-$('#linked-profile-form').submit(function() {
-  var email = $(this).find('input[name="email"]').val();
-  debugger;
-  $.ajax({
-    type: $(this).attr('method'),
-    url: apiDomainURL + $(this).attr('action') + email,
-    headers: {
-      Accept: "application/json"
-    },
-    dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
-  }).success(function(json){
-    console.log("JSON success response", json);
-    var linked_profile_id = json.data[0].id;
-    var linked_profile_firstname = json.data[0].attributes.firstname;
-    $('#linked-profile-form').hide();
-    localStorage.setItem("linked_profile_id", linked_profile_id);
-    localStorage.setItem("linked_profile_firstname", linked_profile_firstname);
-    $('#message').text('Success!').show();
-  }).error(function(json) {
-    console.log (json);
-    $('#message').text('Oops, there was a problem').show();
-  });
-
-  /* stop form from submitting normally */
-  event.preventDefault();
+$('input[type=radio][name=received]').change(function() {
+    if (this.value == 'received') {
+        $('#rating-element').show();
+    }
+    else if (this.value == 'given') {
+        $('#rating-element').hide();
+    }
 });
 
 $('#love-bank').submit(function() {
@@ -152,6 +108,13 @@ $('#entry').submit(function() {
   var occurred_on = $('#date-picker-2').val();
   var rating = $('#rating').val();
   var received = $('input[name=received]:checked', $(this)).val();
+  if (received === 'given') {
+    received = false;
+    rating = null;
+  } else {
+    received = true;
+  }
+
   var keep_private = $('input[name=keep_private]:checked', $(this)).val();
   var description = $(this).find('textarea').val();
   var profile_id = localStorage.getItem("profile_id");
