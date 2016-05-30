809aa6ab30f70d6e337efddb878e831adef8a88f
Jared L Jennings <jared@jaredjennings.org>
2016-05-30 12:17:57 -0500

    New publish commands
    gulp publish-www and gulp publish-me
    This is until we have a react sight or whatever we end up doing.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -168,11 +168,40 @@ gulp.task('deploy', ['build'], () => {
     }))
 });
 
-gulp.task('publish', function() {
+gulp.task('publish-www', function() {
 
   // create a new publisher using S3 options
   // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
   var publisher = awspublish.create(JSON.parse(fs.readFileSync('./aws-credentials.json')));
+
+  // define custom headers
+  var headers = {
+    'Cache-Control': 'max-age=315360000, no-transform, public'
+    // ...
+  };
+
+  return gulp.src('./dist/**')
+      // gzip, Set Content-Encoding headers and add .gz extension
+      .pipe(awspublish.gzip())
+
+      // publisher will add Content-Length, Content-Type and headers specified above
+      // If not specified it will set x-amz-acl to public-read by default
+      .pipe(publisher.publish(headers))
+
+      .pipe(publisher.sync())
+
+      // create a cache file to speed up consecutive uploads
+      .pipe(publisher.cache())
+
+      // print upload updates to console
+      .pipe(awspublish.reporter());
+});
+
+gulp.task('publish-me', function() {
+
+  // create a new publisher using S3 options
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
+  var publisher = awspublish.create(JSON.parse(fs.readFileSync('./aws-credentials-me.json')));
 
   // define custom headers
   var headers = {
