ed270a4847b4bc1fc63daa333acf1e2c1b231c64
Jared L Jennings <jared@jaredjennings.org>
2016-05-31 10:37:08 -0500

    Removed extra publish statement
    It's not right and so I'm removing it.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -168,7 +168,7 @@ gulp.task('deploy', ['build'], () => {
     }))
 });
 
-gulp.task('publish-www', function() {
+gulp.task('publish', function() {
 
   // create a new publisher using S3 options
   // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
@@ -195,33 +195,4 @@ gulp.task('publish-www', function() {
 
       // print upload updates to console
       .pipe(awspublish.reporter());
-});
-
-gulp.task('publish-me', function() {
-
-  // create a new publisher using S3 options
-  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
-  var publisher = awspublish.create(JSON.parse(fs.readFileSync('./aws-credentials-me.json')));
-
-  // define custom headers
-  var headers = {
-    'Cache-Control': 'max-age=315360000, no-transform, public'
-    // ...
-  };
-
-  return gulp.src('./dist/**')
-    // gzip, Set Content-Encoding headers and add .gz extension
-    .pipe(awspublish.gzip())
-
-    // publisher will add Content-Length, Content-Type and headers specified above
-    // If not specified it will set x-amz-acl to public-read by default
-    .pipe(publisher.publish(headers))
-
-    .pipe(publisher.sync())
-
-    // create a cache file to speed up consecutive uploads
-    .pipe(publisher.cache())
-
-    // print upload updates to console
-    .pipe(awspublish.reporter());
 });
